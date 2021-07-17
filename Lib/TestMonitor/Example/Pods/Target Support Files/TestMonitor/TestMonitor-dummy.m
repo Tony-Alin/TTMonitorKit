14db17722fa7fe84d1fa4ace76cc62825dc60122
Tony-Alin <67528537+Tony-Alin@users.noreply.github.com>
2021-07-17 22:31:14 +0800

    Add files via upload

---
+++ Lib/TestMonitor/Example/Pods/Target Support Files/TestMonitor/TestMonitor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestMonitor : NSObject
+@end
+@implementation PodsDummy_TestMonitor
+@end
---
+++ Lib/TestMonitor/Example/TestMonitor/XXViewController.h
@@ -0,0 +1,13 @@
+//
+//  XXViewController.h
+//  TestMonitor
+//
+//  Created by Tony-Alin on 07/17/2021.
+//  Copyright (c) 2021 Tony-Alin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XXViewController : UIViewController
+
+@end
